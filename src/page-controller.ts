59faea5ea04da4cd5a6e41b54f046a886d3eb694
kaanyagci <kaan.yagci@makepad.fr>
2023-05-17 02:45:32 +0200

    :sparkles: event listeners based solution implementing
    session is got using event listeners

--- src/page-controller.ts
+++ src/page-controller.ts
@@ -4,6 +4,13 @@ import { NEW_CHAT_BUTTON_SELECTOR } from "./selectors";
 export const BASE_URL = 'https://chat.openai.com';
 export const CHAT_URL = `${BASE_URL}/chat`;
 export const LOGIN_PAGE_URL = `${BASE_URL}/auth/login`;
+export const SESSION_API_URL = `${BASE_URL}/api/auth/session`;
+export const BACKEND_API_BASE_URL = `${BASE_URL}/backend-api`;
+export const MODELS_API_URL = `${BACKEND_API_BASE_URL}/models`;
+export const ACCOUNTS_CHECK_API_URL = `${BACKEND_API_BASE_URL}/accounts/check`;
+export const CONVERSATION_HISTORY_API_URL = `${BACKEND_API_BASE_URL}/conversations`;
+export const BETA_FEATURES_API_URL = `${BACKEND_API_BASE_URL}/settings/beta_features`;
+
 export class PageController {
     readonly #page: Page;
 
@@ -30,5 +37,14 @@ export class PageController {
         } catch {
             return false;
         }
+    }
+
+    /**
+     * Sleep for given number of milliseconds
+     * @param timeout The number of milliseconds to sleep for
+     */
+    async sleep(timeout: number) {
+        console.debug(`Will sleep for ${timeout}`);
+        return this.#page.waitForTimeout(timeout);
     }
 }
