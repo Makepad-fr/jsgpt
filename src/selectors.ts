25164f01dd19dc8b0e2426f31e46e4d128d0e831
kaanyagci <kaan.yagci@makepad.fr>
2023-05-17 02:33:46 +0200

    :sparkles: pass the announcement dialog after logged in

--- src/selectors.ts
+++ src/selectors.ts
@@ -3,3 +3,6 @@ export const EMAIL_INPUT_SELECTOR = "//input[contains(@id,'username')]";
 export const PASSWORD_INPUT_SELECTOR = "//input[contains(@id,'password')]";
 export const CONTINUE_BUTTON_SELECTOR = "text='Continue'";
 export const NEW_CHAT_BUTTON_SELECTOR = "text='New chat'";
+export const DIALOG_SELECTOR = "//div[contains(@role,'dialog')]";
+export const NEXT_BUTTON_SELECTOR = "text='Next'"
+export const DONE_BUTTON_SELECTOR = "text='Done'"
