7b7b31516edb83bf2052e79937a2d5b8f2b8ad32
kaanyagci <kaan.yagci@makepad.fr>
2023-05-17 02:48:48 +0200

    :sparkles: new selectors added for challenge

--- src/selectors.ts
+++ src/selectors.ts
@@ -3,6 +3,10 @@ export const EMAIL_INPUT_SELECTOR = "//input[contains(@id,'username')]";
 export const PASSWORD_INPUT_SELECTOR = "//input[contains(@id,'password')]";
 export const CONTINUE_BUTTON_SELECTOR = "text='Continue'";
 export const NEW_CHAT_BUTTON_SELECTOR = "text='New chat'";
+export const CHALLENGE_DIV_SELECTOR = "div#challenge-stage"
+export const IFRAME_SELECTOR = "//iframe[contains(@title, 'Widget containing a Cloudflare security challenge')]"
+export const CHECKBOX_SELECTOR = "//input[@type='checkbox']"
+export const VERIFY_HUMAN_SELECTOR = "text='Verify you are human'";
 export const DIALOG_SELECTOR = "//div[contains(@role,'dialog')]";
 export const NEXT_BUTTON_SELECTOR = "text='Next'"
 export const DONE_BUTTON_SELECTOR = "text='Done'"
